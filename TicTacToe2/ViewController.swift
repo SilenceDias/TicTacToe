23d1f148b8c4074cdfaa6a565028a7eb3424cb7b
SilenceDias <11dias02@mail.ru>
2023-10-19 19:09:16 +0600

    Initial Commit

---
+++ TicTacToe2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToe2
+//
+//  Created by Диас Мухамедрахимов on 19.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
